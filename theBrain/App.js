3cbd20f17ecda315423a839ac883f761bea45c10
Jamison Bunge <JamisonBunge@outlook.com>
2020-02-21 12:26:37 -0500

    set up the backend boilerplate

---
+++ theBrain/App.js
@@ -0,0 +1,57 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+//const Places = require('./DataSources/Places')
+const path = require('path')
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+
+const schema = gql`
+type Query {
+	test: String,
+}
+`;
+
+const resolvers = {
+    Query: {
+        test: () => { return "MuddBot 3.0" },
+    },
+    // ,
+};
+
+
+
+
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers//,
+    // dataSources: () => ({
+    // 	Places: new Places,
+    // })
+});
+
+// server.listen().then(({ url }) => {
+// 	console.log(`Server ready at ${url}`);
+// });
+
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+// 	console.log(`🚀 Server ready at ${url}`);
+// });
+
+
+//
+app.use(express.static('public'))
+
+//redirect everything that isnt to /graphql to the index.thml of the react app build
+
+
+// Add the Apollo Server’s middleware
+server.applyMiddleware({ app })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+app.listen({ port: process.env.PORT || 5000 }, () => console.log(`Apollo Server is listening on ${server.graphqlPath}`))
