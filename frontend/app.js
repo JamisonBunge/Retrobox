6f3551643ff5550dc50b430295564ee87cd935fe
Jamison Bunge <JamisonBunge@outlook.com>
2020-04-10 16:16:30 -0400

    started transisioning to proper dialog card

--- frontend/app.js
+++ frontend/app.js
@@ -80,7 +80,11 @@ async function getCommand(keyword) {
         .then(d => d[keyword])
         .then((response) => {
             //here the object has been stripped down to be specific for each call
+
+            //populate card with info
             func(response, keyword)
+
+
         })
 }
 
@@ -106,14 +110,23 @@ function formatResponseForecast(response, offset) {
     Low: ${response.temp_min}<br>`
 }
 
+function lol() { console.log("lol") }
 function handleWeatherNow(response, keyword) {
 
     let greaterDetails = formatResponse(response)
-
+    document.getElementById('serverprompt').innerHTML = `${response.response} `
     document.getElementById('card-header').innerHTML = `${keyword} endpoint`
     document.getElementById('card-title').innerHTML = `${response.response} `
     document.getElementById('card-subtitle').innerHTML = new Date().formatMMDDYYYY()
     document.getElementById('card-text').innerHTML = greaterDetails
+
+
+    function displayTempOutputCard() {
+        document.getElementById('tempoutput').style.display = "block"
+
+    }
+
+    displayTempOutputCard()
 
 }
 
@@ -134,3 +147,45 @@ function handleWeatherForecast(response, keyword) {
     document.getElementById('card-text').innerHTML = greaterDetails
 
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Handler when the DOM is fully loaded
+    console.log('this loads when the com is loaded')
+});
+
+function dialogprogression() {
+
+    //this needs to be changed to use the ID of the button thats pressing it
+    //for now it will be hardcoded for weathernow
+    document.getElementById('tempoutput').style.display = "none"
+    document.getElementById('serverprompt').innerHTML = ""
+
+    let parsedCmd = 'weatherNow'
+
+
+    //1 start listening for voice
+    //a. init voice visualizer  on output div
+    //c, init js that does this bullshit
+    //d. after the pause, get parsed cmd
+    //e. unmount voice visualizer
+    //f. mount loading animation on output div
+
+
+    //getCommand(parsedCmd)
+
+
+}
+
+
+    //1 start listening for voice
+        //a. init voice visualizer  on output div
+        //c, init js that does this bullshit
+        //d. after the pause, get parsed cmd
+        //e. unmount voice visualizer
+        //f. mount loading animation on output div
+
+    //2 run getCommand(parsedCmd)
+        //wait for async call to get
+        //set response on dialog card
+        //set output div to html for output card
+            //^THIS IS TODO. for it just 'shows' the hidden card
