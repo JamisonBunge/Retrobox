cb2edceb6b00be7aef9bf470b2ef2510b41d154a
Allan Delarosa <ajd2215@columbia.edu>
2020-05-09 20:50:50 -0400

    Cleaned up js a bit; sliders should work for the two sample songs now

--- frontend/assets/js/sliders.js
+++ frontend/assets/js/sliders.js
@@ -1,15 +1,15 @@
 var timer = null;
 var switched = false;
 
-document.getElementById('low').oninput = function() {
+document.getElementById('low').oninput = function () {
     sliderval(document.getElementById('low'), document.getElementById('low-display'))
 }
 
-document.getElementById('mid').oninput = function() {
+document.getElementById('mid').oninput = function () {
     sliderval(document.getElementById('mid'), document.getElementById('mid-display'))
 }
 
-document.getElementById('high').oninput = function() {
+document.getElementById('high').oninput = function () {
     sliderval(document.getElementById('high'), document.getElementById('high-display'))
 }
 
@@ -26,54 +26,39 @@ function setNewSource() {
     high = 7 - document.getElementById('high').value + 1
 
     // change audio source for this specific configuration
-    if ( low == 7 && mid == 1 && high == 1 && !switched) {
+    if (low == 7 && mid == 1 && high == 1 && !switched) {
         console.log('switching audio source')
         switched = true
 
         players = document.querySelectorAll('audio')
-        if (!players[0].paused) {
-            // switch audio source and resume playing
-            for (let player of players) {
-                player.pause()
-            }
 
-            for (let player of players) {
-                player.src = 'assets/music/take-on-me-clip-processed.wav'
-            }
+        source = players[0].src
+        newsource = source.substring(0, source.length - 4) + '-processed.wav'
 
-            for (let player of players) {
-                player.play()
-            }
-        } else {
-            // just change audio source
-            for (let player of players) {
-                player.src = 'assets/music/take-on-me-clip-processed.wav'
-            }
-        }
+        switchSource(players, newsource)
+
     } else if (switched) {
         console.log('switching audio source')
 
         switched = false
 
         players = document.querySelectorAll('audio')
-        if (!players[0].paused) {
-            // switch audio source and resume playing
-            for (let player of players) {
-                player.pause()
-            }
 
-            for (let player of players) {
-                player.src = 'assets/music/take-on-me-clip.wav'
-            }
+        source = players[0].src
+        newsource = source.substring(0, source.length - ('-processed.wav'.length)) + '.wav'
 
-            for (let player of players) {
-                player.play()
-            }
-        } else {
-            // just change audio source
-            for (let player of players) {
-                player.src = 'assets/music/take-on-me-clip.wav'
-            }
-        }
+        switchSource(players, newsource)
+    }
+}
+
+function switchSource(players, newsource) {
+    if (!players[0].paused) {
+        // if playing, switch audio source and resume playing
+        for (let player of players) player.pause()
+        for (let player of players) player.src = newsource
+        for (let player of players) player.play()
+    } else {
+        // if not playing, just change audio source
+        for (let player of players)player.src = newsource
     }
 }
