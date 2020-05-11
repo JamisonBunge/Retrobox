78e5531c1e394086bf88f6947108ae85b0f32abd
Allan Delarosa <ajd2215@columbia.edu>
2020-05-10 22:02:50 -0400

    Added slight styling to slider displays

--- frontend/assets/js/button-sound.js
+++ frontend/assets/js/button-sound.js
@@ -5,4 +5,15 @@ document.querySelectorAll('button').forEach(button => {
         clickSound.volume = 0.5
         clickSound.play()
     })
-  })
+})
+
+// clicking sound really doesn't sound good, find a new one
+// sliders = document.getElementsByClassName('retro-slider')
+// for (let slider of sliders) {
+//     slider.oninput = () => {
+//         let clickSound = new Audio('assets/audio/button-click.wav')
+//         // default click is too loud
+//         clickSound.volume = 0.1
+//         clickSound.play()
+//     }
+// }
